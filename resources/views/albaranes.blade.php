523ccc7637d5d198a30e9b3b9ee43d85fca57e2e
JuanBaronV <juan.baronviciana@gmail.com>
2024-10-22 13:03:04 +0200

    modifico el texto si no tiene albaranes

--- resources/views/albaranes.blade.php
+++ resources/views/albaranes.blade.php
@@ -2,34 +2,39 @@
 @section('content')
 
 <div class="container my-4 p-4" style="color:black; background-color: white; border-radius: 1em;">
-    @foreach($albaranesPorProveedor as $codigo => $albaranes)
-    <h1 class="h4">Albaranes del Proveedor: {{ $albaranes->first()->proveedor ? $albaranes->first()->proveedor->NOMBRE : 'Proveedor no disponible' }}</h1>
-        
-        <div class="table-responsive">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>TIPO</th>
-                        <th>NÚMERO DE ALBARÁN</th>
-                        <th>FECHA</th>
-                        <th>Mostrar</th> <!-- Columna para las acciones -->
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($albaranes as $albaran)
-                    <tr>
-                        <td>{{ $albaran->TIPO }}</td>
-                        <td>{{ $albaran->NUMERO }}</td>
-                        <td>{{ $albaran->FECHA }}</td>
-                        <td>
-                            <a href="{{ route('mostrarAlbaran', $albaran->NUMERO) }}" class="btn btn-primary btn-sm">Ver Detalles</a>
-                        </td> <!-- Botón para ver detalles -->
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    @endforeach
+    <h2>Albaranes</h2>
+    @if($albaranesPorProveedor->isEmpty())
+        <h1 class="h4">Todavía no tiene albaranes</h1>
+    @else
+        @foreach($albaranesPorProveedor as $codigo => $albaranes)
+            <h1 class="h4">Albaranes del Proveedor: {{ $albaranes->first()->proveedor ? $albaranes->first()->proveedor->NOMBRE : 'Proveedor no disponible' }}</h1>
+            
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>TIPO</th>
+                            <th>NÚMERO DE ALBARÁN</th>
+                            <th>FECHA</th>
+                            <th>Mostrar</th> <!-- Columna para las acciones -->
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($albaranes as $albaran)
+                            <tr>
+                                <td>{{ $albaran->TIPO }}</td>
+                                <td>{{ $albaran->NUMERO }}</td>
+                                <td>{{ $albaran->FECHA }}</td>
+                                <td>
+                                    <a href="{{ route('mostrarAlbaran', $albaran->NUMERO) }}" class="btn btn-primary btn-sm">Ver Detalles</a>
+                                </td> <!-- Botón para ver detalles -->
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @endforeach
+    @endif
 </div>
 
 @endsection
